6b69ada653baeb584e4fc0bf26a43641ad638102
Tae.Garam <drasla@naver.com>
2023-05-26 23:15:46 +0900

    04. 컴포넌트 - 조건과 반복

--- src/App.js
+++ src/App.js
@@ -1,30 +1,26 @@
-import {Component} from "./core/heropy";
+import { Component } from "./core/heropy";
 
 export default class App extends Component {
     constructor() {
         super({
             state: {
-                inputText: ""
-            }
+                fruits: [
+                    { name: "Apple", price: 1000 },
+                    { name: "Banana", price: 2000 },
+                    { name: "Cherry", price: 3000 },
+                ],
+            },
         });
-
     }
 
     render() {
-        this.el.classList.add("search");
         this.el.innerHTML = `
-            <input />
-            <button>Click!</button>
-        `;
-
-        const inputEl = this.el.querySelector("input")
-        inputEl.addEventListener("input", () => {
-            this.state.inputText = inputEl.value;
-        });
-
-        const buttonEl = this.el.querySelector("button")
-        buttonEl.addEventListener("click", () => {
-            console.log(this.state.inputText);
-        })
+        <h1>Fruits</h1>
+<ul>
+${this.state.fruits
+    .filter(fruit => fruit.price < 3000)
+    .map(fruit => `<li>${fruit.name}</li>`)
+    .join("")}
+</ul>`;
     }
 }
